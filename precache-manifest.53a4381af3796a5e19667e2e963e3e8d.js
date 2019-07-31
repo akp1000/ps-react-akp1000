ba64f713fad4982d3dccb4c7df1fe1bb7777eeae
Kabir Patel <kabirpatel@kabirs-mbp.mynet>
2019-07-31 20:23:57 +0100

    Updates

---
+++ precache-manifest.53a4381af3796a5e19667e2e963e3e8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5befa2945b49846b5c12c76b5bb1076",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e267f6c183d5a8eb7a3",
+    "url": "/static/css/2.52738377.chunk.css"
+  },
+  {
+    "revision": "beb31ef0d6b0155875e7",
+    "url": "/static/css/main.c5424e77.chunk.css"
+  },
+  {
+    "revision": "2e267f6c183d5a8eb7a3",
+    "url": "/static/js/2.1822c141.chunk.js"
+  },
+  {
+    "revision": "beb31ef0d6b0155875e7",
+    "url": "/static/js/main.3f1f2ec3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
